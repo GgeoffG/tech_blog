41c1ed8a1acb78a12946e2cde502fe2ac7d39115
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-05-11 09:27:50 -0700

    progress commit

---
+++ seeds/user-seeds.js
@@ -0,0 +1,27 @@
+const { User } = require('../models');
+
+const userData = [
+  {
+    name: 'Sal',
+    email: 'sal@hotmail.com',
+    password: 'password12345',
+  },
+  {
+    name: 'Lernantino',
+    email: 'lernantino@gmail.com',
+    password: 'password12345',
+  },
+  {
+    name: 'Amiko',
+    email: 'amiko2k20@aol.com',
+    password: 'password12345',
+  },
+];
+
+const seedUsers = () =>
+  User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+module.exports = seedUsers;
